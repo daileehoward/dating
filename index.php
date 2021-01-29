82c342922bc60711c9b0125e282a7cb66a49269f
Dailee Howard <Dhoward18@mail.greenriver.edu>
2021-01-28 23:10:23 -0500

    Added directory structure and image

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    // Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    // Instantiate Fat-Free
+    $f3 = Base::instance();
+
+    // Turn on Fat-Free error reporting
+    $f3->set('DEBUG', 3);
+
+    //Define a default route (home page)
+    $f3->route('GET /', function ()
+    {
+        echo "Home Page";
+        // Display the view
+        //$view = new Template();
+        //echo $view->render('views/home.html');
+    });
+
+    $f3->run();
