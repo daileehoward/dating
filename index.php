9b00d8709f9c8ba90d52eb37b31455363666381b
Dailee Howard <Dhoward18@mail.greenriver.edu>
2021-02-12 01:00:01 -0500

    Added views pages to project

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
     //Require the autoload file
     require_once('vendor/autoload.php');
+    require_once('model/data-layer.php');
 
     // Instantiate Fat-Free
     $f3 = Base::instance();
@@ -18,6 +19,30 @@
         // Display the view
         $view = new Template();
         echo $view->render('views/home.html');
+    });
+
+    //Define a default route (personal information page)
+    $f3->route('GET /personal-information', function ()
+    {
+        // Display the view
+        $view = new Template();
+        echo $view->render('views/personal-information-form.html');
+    });
+
+    //Define a default route (profile page)
+    $f3->route('GET /profile', function ()
+    {
+        // Display the view
+        $view = new Template();
+        echo $view->render('views/profile-form.html');
+    });
+
+    //Define a default route (interests page)
+    $f3->route('GET /interests', function ()
+    {
+        // Display the view
+        $view = new Template();
+        echo $view->render('views/interests-form.html');
     });
 
     $f3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+    function getIndoorInterests()
+    {
+        return array("sleep", "steal treats", "window watching", "hide and seek", "learn new tricks", "scratch furniture",
+            "grooming time", "cuddle");
+    }
+
+    function getOutdoorInterests()
+    {
+        return array("fetch", "car ride", "sun bathing", "visit the park", "swimming", "chase wildlife", "get muddy",
+            "jump fences");
+    }
